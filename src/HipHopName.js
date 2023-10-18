6141ecf5be9205e236843cb65e72ed46ca1796ae
cariocaphil <cariocaphil@gmail.com>
2023-10-18 13:38:27 +0200

    adjust prompt

--- src/HipHopName.js
+++ src/HipHopName.js
@@ -1,6 +1,6 @@
 const generateHipHopName = async (name, additionalInfo, prefix) => {
   const apiKey = process.env.REACT_APP_OPENAI_API_KEY;
-  const prompt = `Transform the name "${name}" into a hip hop star name:`;
+  const prompt = `Transform the name "${name}" into a hip hop star name, and consider the additional information about the person having that name, "${additionalInfo}", and the prefix chosen "${prefix}" :`;
 
   try {
     const response = await fetch('https://api.openai.com/v1/engines/davinci/completions', {
